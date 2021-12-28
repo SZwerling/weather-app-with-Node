b1a67da4610a2db7f9a7c38fa3f0da9a3c75f263
Seth Zwerling <sazwerling@gmail.com>
2021-12-28 12:46:40 -0600

    error handling

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const { randomInt } = require('crypto');
+
 const request = require('request');
 
+// GEOCODING, client supplies address, api call to find lat and long, then req to weather stack
 
-const url = 'http://api.weatherstack.com/current?access_key=ea9c083d5d840352627cce481d258e1c&query=37.8267,-122.4233'
+
+const url = 'http://api.weatherstack.com/current?access_key=ea9c083d5d840352627cce481d258e1c&query=37.8267,-122.4233&units=f'
 
 request({ url: url, json: true }, (error, response) => {
-    const data = response.body.current
-    console.log(`It is currently ${data.temperature} degrees celcius. There is a ${data.precip * 100}% chance of rain`)
+    if(error){
+        console.log('Unable to connect to weather stack. Ja Ja.')
+    } else if(response.body.error){
+        console.log('Unable to get what you want.')
+    } else {
+        const data = response.body.current;
+        console.log(`It is currently ${data.temperature} degrees celcius. There is a ${data.precip * 100}% chance of rain`);
+        // console.log(data)
+    }
+  
 })
 
 
+const otherUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/gaudalajara.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoic2xpbWJpZ2d5IiwiYSI6ImNrbXFvcjd2azAwMDgybm50eXJocWR4cWsifQ.VzNz24swKwxCgnHWDoZGsw&limit=1'
+
+request({ url: otherUrl, json: true }, (error, response) => {
+    if(error){
+        console.log('Unable to connect to mapbox.')
+    } else if(!response.body.features || response.body.features.length === 0){
+        console.log('Unable to get what you want.')
+    }  else {
+        const data = response.body.features[0].center;
+        console.log(`The latitude is ${data[1]} and the longitude is ${data[0]}`)
+    }
+   
+})
 
 
+// data.weather_descriptions[0]
 
-// ea9c083d5d840352627cce481d258e1c
+// ea9c083d5d840352627cce481d258e1c    api key
 
 // http://api.weatherstack.com/
