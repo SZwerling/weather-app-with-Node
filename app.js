64a4b48830f1727bceb8f43d6f4545c83ca23e88
unknown <sazwerling@gmail.com>
2022-01-11 12:15:17 -0600

    destructuring and property shortand

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ const forecast = require('./utils/forecast');
 //only geocode if a location was provided
 //test work with a couple of locations
 
-const location = process.argv[2]
+const local = process.argv[2]
 
 
-geocode(location, (error, data) => {
-    if(!location){
+geocode(local, (error, { latitude, longitude, location }) => {
+    if(!local){
       return console.log('Please supply a valid location.')
     }
     if(error){
       return console.log(error)
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if(error){
         return console.log(error)
       }
-      console.log(data.location)
+      console.log(location)
       console.log(forecastData)
     })
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,14 +2,15 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=ea9c083d5d840352627cce481d258e1c&query=' + latitude + ',' + longitude + '&units=f';
-    request({ url: url, json: true}, (error, response) => {
+    
+    request({ url, json: true}, (error, { body }) => {
         if(error){
             callback('Unable to connect to weather stack.')
-        } else if(response.body.error){
+        } else if(body.error){
             callback('Unable to find location.')
         } else {
-            const data = response.body.current;
-            callback(undefined, `It is currently ${data.temperature} degrees fareinheight. There is a ${data.precip * 100}% chance of rain`) 
+            const data = body.current;
+            callback(undefined, `It is currently ${data.temperature} degrees fahrenheit. There is a ${data.precip * 100}% chance of rain`) 
         }
     } )
 }
